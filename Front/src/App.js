6525b9ccef7a73e1c8392629712ed7653da6528c
MattDomi <mattelzo@yahoo.com>
2022-12-03 16:37:12 -0800

    Front again

---
+++ Front/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from "react";
+import Axios from "axios"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://proj174.herokuapp.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
